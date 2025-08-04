29de9e4586ffb43831ddecb12152cf6e05a9a7fa
Sky Li <lyxsky@hotmail.com>
2025-08-04 14:59:27 -0400

    Remove visualization utilities and related imports from the visualization module, including latent space plotting functions and metrics.

--- src/visualization/__init__.py
+++
@@ -1,9 +0,0 @@
-from .latent_space import plot_latent_alignment, plot_latent_biology
-from .metrics import plot_imputation_metrics, plot_feature_comparison
-
-__all__ = [
-    "plot_latent_alignment", 
-    "plot_latent_biology",
-    "plot_imputation_metrics", 
-    "plot_feature_comparison"
-] 
